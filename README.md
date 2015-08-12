45378bcd133eb3fce208fb32a9b9baac333590fa
donguriie <donguriie@gmail.com>
2015-08-12 13:13:33 +0900

    initial: local test done.

---
+++ README.md
@@ -0,0 +1,4 @@
+Backbone Study Chapter 4
+========================
+
+Backbone.js 애플리케이션 개발하기 -- 4장
---
+++ js/app.js
@@ -0,0 +1,6 @@
+var app = app || {};
+var ENTER_KEY = 13;
+
+jQuery(function(){
+  new app.AppView();
+});
